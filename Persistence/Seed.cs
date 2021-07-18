2b13201bcb37bf2b7288c566e1a74d29c4db2bf5
gold <goldencrosslabs@gmail.com>
2021-07-17 22:20:50 -0500

    Added skeleton controller and entities

---
+++ Persistence/Seed.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Activities.Any()) return;
+
+            var activities = new List<Activity>
+            {
+                new Activity
+                {
+                    Title = "Past Activity 1",
+                    Date = DateTime.Now.AddMonths(-2),
+                    Description = "Activity 2 months ago",
+                    Category = "drinks",
+                    City = "Los Angeles",
+                    Venue = "Bar",
+                },
+                new Activity
+                {
+                    Title = "Past Activity 2",
+                    Date = DateTime.Now.AddMonths(-1),
+                    Description = "Activity 1 month ago",
+                    Category = "culture",
+                    City = "Saint Paul",
+                    Venue = "Summer Fest",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 1",
+                    Date = DateTime.Now.AddMonths(1),
+                    Description = "Activity 1 month in future",
+                    Category = "culture",
+                    City = "San Jose",
+                    Venue = "Natural History Museum",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 2",
+                    Date = DateTime.Now.AddMonths(2),
+                    Description = "Activity 2 months in future",
+                    Category = "music",
+                    City = "Edina",
+                    Venue = "ED Park",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 3",
+                    Date = DateTime.Now.AddMonths(3),
+                    Description = "Activity 3 months in future",
+                    Category = "drinks",
+                    City = "Eden Praire",
+                    Venue = "Another Bar",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 4",
+                    Date = DateTime.Now.AddMonths(4),
+                    Description = "Activity 4 months in future",
+                    Category = "drinks",
+                    City = "San Francisco",
+                    Venue = "Yet another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 5",
+                    Date = DateTime.Now.AddMonths(5),
+                    Description = "Activity 5 months in future",
+                    Category = "drinks",
+                    City = "New York City",
+                    Venue = "Just another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 6",
+                    Date = DateTime.Now.AddMonths(6),
+                    Description = "Activity 6 months in future",
+                    Category = "music",
+                    City = "Madison",
+                    Venue = "Roundhouse Camden",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 7",
+                    Date = DateTime.Now.AddMonths(7),
+                    Description = "Activity 2 months ago",
+                    Category = "travel",
+                    City = "Milwaukee",
+                    Venue = "Somewhere on the Thames",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 8",
+                    Date = DateTime.Now.AddMonths(8),
+                    Description = "Activity 8 months in future",
+                    Category = "film",
+                    City = "Detroit",
+                    Venue = "Cinema",
+                }
+            };
+
+            await context.Activities.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
